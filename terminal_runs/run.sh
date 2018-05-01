75d36db73af8eeb0a2c7a90aaf4b6e2c4548830b
JacksonMaxfield <jacksonb@alleninstitute.org>
2018-05-01 14:28:10 -0700

    added run.sh

---
+++ terminal_runs/run.sh
@@ -0,0 +1,4 @@
+docker run -t \
+	-v /allen/aics/modeling/jacksonb/:/allen/aics/modeling/jacksonb/ \
+	lf_predict label_free_predict \
+	/allen/aics/modeling/jacksonb/projects/pytorch_fnet/terminal_runs/config.json
